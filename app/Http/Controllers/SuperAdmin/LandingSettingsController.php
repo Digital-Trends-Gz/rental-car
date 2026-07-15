bb52070da200ffabbefee59170162335514d288e
ziadaboalkamar <ziad-lovelyboy@hotmail.com>
2026-07-15 14:59:32 +0300

    update

--- app/Http/Controllers/SuperAdmin/LandingSettingsController.php
+++ app/Http/Controllers/SuperAdmin/LandingSettingsController.php
@@ -618,6 +618,15 @@ class LandingSettingsController extends Controller
             'content_type' => $request->headers->get('content-type'),
             'content_length' => $request->headers->get('content-length'),
             'raw_files_superglobal' => $_FILES,
+            'input_summary' => collect($request->all())->map(function ($value) {
+                if (is_string($value)) {
+                    return ['type' => 'string', 'length' => strlen($value), 'preview' => substr($value, 0, 100)];
+                }
+                if (is_array($value)) {
+                    return ['type' => 'array', 'count' => count($value)];
+                }
+                return ['type' => get_debug_type($value)];
+            })->all(),
             'has_files' => $request->hasFile('hero_direct_file')
                 || $request->hasFile('hero_locale_direct_files')
                 || $request->hasFile('feature_card_direct_files')
